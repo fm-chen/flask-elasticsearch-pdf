511e3a765e61879072a7fb844e534f1c93aca6a0
Fumian Chen <475615524@qq.com>
2021-07-01 14:58:00 -0400

    Update uploader.py

--- web/uploader.py
+++ web/uploader.py
@@ -8,9 +8,9 @@ def pdf_loader(pdf_file, filename):
     headers = {'content-type': 'application/cbor'}
     index = 'my-index-07'
     _id = filename + "_" + str(ct)
-    # url = 'http://localhost:9200/%s/_doc/%s?pipeline=cbor-attachment' % (index, _id)
+    url = 'http://localhost:9200/%s/_doc/%s?pipeline=cbor-attachment' % (index, _id)
     # url = 'elasticsearch:9200/%s/_doc/%s?pipeline=cbor-attachment' % (index, _id)
-    url = 'http://147.182.174.38:9200/%s/_doc/%s?pipeline=cbor-attachment' % (index, _id)
+    
     try:
         doc = {
             'data': pdf_file.read(),
