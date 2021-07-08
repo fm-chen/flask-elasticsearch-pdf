69e82348869311854f888ad2919ae3aa6143d604
Fumian Chen <475615524@qq.com>
2021-07-08 00:51:52 -0400

    Update init.py

--- web/init.py
+++ web/init.py
@@ -3,21 +3,20 @@ import os
 from uploader import pdf_loader
 import requests
 
-# url = "http://localhost:9200/_ingest/pipeline/cbor-attachment"
-# url = "http://elasticsearch:9200/_ingest/pipeline/cbor-attachment"
-url = "http://147.182.174.38:9200/_ingest/pipeline/cbor-attachment"
+url = "http://localhost:9200/_ingest/pipeline/cbor-attachment"
+
 
 headers = {'content-type': 'application/json'}
 
 data = '{"description": "Extract attachment information", "processors": [{"attachment": {"field": "data"}}]}'
 
-'''
+
 r = requests.put(
     url,
     data=data,
     headers=headers
 )
-'''
+
 
 os.chdir("./pdf")
 for file in glob.glob("*.pdf"):
